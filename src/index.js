38e00c3a552b40859c194c07398dc9f825b4071e
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2024-10-12 11:17:48 +0200

    feat: add readline and update exit for file manager

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
+import readline from 'node:readline/promises';
+import { stdin, stdout } from 'node:process';
+
 import { sayHiToUser } from "./operations/sayHiToUser.js";
 import { sayByeToUser } from "./operations/sayByeToUser.js";
+import { readLineInterface } from "./utils/readlineInterface.js";
+
+const rl = readline.createInterface( { input: stdin, output: stdout } );
+readLineInterface.readlineInterface = rl;
 
 const fileManager = async() => {
   await sayHiToUser();
-  await sayByeToUser();
+
+  rl.prompt(); 
+
+  rl.on('line', async (input) => {
+    if (input.trim() === '.exit') {
+      rl.close();
+    } else {
+      console.log(`Invalid input: ${input}`);
+      rl.prompt();
+    }
+  });
+
+  rl.on('close', async() =>{
+    await sayByeToUser();
+    process.exit(0);
+  })
 }
 
 fileManager();
